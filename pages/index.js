5659832660f3e93f0f651d2289e04c0d38ef1d36
chavincc <cc6chavin@gmail.com>
2020-06-25 15:07:09 +0700

    feat(login): mark up and style component

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
-const ENDPOINT = process.env.ENDPOINT || "http://127.0.0.1:4001";
+import { useRouter } from "next/router";
 
-const App = () => {
-  const [response, setResponse] = useState("");
+// import socketIOClient from "socket.io-client";
+// const ENDPOINT = process.env.ENDPOINT || "http://127.0.0.1:4001";
 
+const Home = () => {
+  const router = useRouter();
   useEffect(() => {
-    (async () => {
-      const socket = socketIOClient(ENDPOINT);
-      socket.emit("INTRODUCE", "matt");
-      socket.on("FromAPI", (data) => {
-        setResponse(data);
-      });
-    })();
-  }, []);
+    router.push("/login");
+  }, [router]);
+  return <>loading..</>;
 
-  return (
-    <p>
-      It's <time dateTime={response}>{response}</time>
-    </p>
-  );
+  // const [response, setResponse] = useState("");
+  // useEffect(() => {
+  //   (async () => {
+  //     const socket = socketIOClient(ENDPOINT);
+  //     socket.emit("INTRODUCE", "matt");
+  //   })();
+  // }, []);
+  // return (
+  //   <p>
+  //     It's <time dateTime={response}>{response}</time>
+  //   </p>
+  // );
 };
 
-export default App;
+export default Home;
---
+++ pages/login.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { deepclone } from "../src/services/util";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInput = (e) => {
+    const newFormData = deepclone(formData);
+    formData[e.target.name] = e.target.value;
+    setFormData(newFormData);
+  };
+
+  return (
+    <section id="login">
+      <div className="login-container">
+        <form onSubmit={handleLogin}>
+          <div className="logo">
+            <img src="images/logo.svg" alt="shall unite logo" />
+          </div>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            autoComplete={false}
+          />
+          <button>Let's Go</button>
+        </form>
+        <img
+          class="login-main-img"
+          src="images/home-graphic.jpg"
+          alt="vector of people"
+        />
+      </div>
+      <style jsx>
+        {`
+          #login {
+            display: flex;
+            justify-content: center;
+          }
+
+          .login-container {
+            max-width: 1100px;
+            min-width: 90vw;
+            display: flex;
+          }
+
+          form {
+            width: 445px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+
+          .logo {
+            margin-bottom: 50px;
+          }
+
+          input,
+          ::placeholder {
+            font-style: normal;
+            font-weight: 500;
+            font-size: 15px;
+            color: #bcb8b8;
+          }
+          input {
+            color: #303030;
+            width: 341px;
+            height: 50px;
+
+            background: #fcfbfe;
+            border: 1px solid #e5e6ef;
+            border-radius: 100px;
+            outline: none;
+
+            margin-bottom: 30px;
+            padding-left: 30px;
+          }
+
+          button {
+            width: 370px;
+            height: 60px;
+            border: none;
+            background: #216efb;
+            border-radius: 100px;
+            font-weight: bold;
+            font-size: 21px;
+            line-height: 31px;
+            color: #ffffff;
+            cursor: pointer;
+            transition: 0.2s;
+            margin-bottom: 8px;
+            outline: none;
+          }
+          button:hover {
+            box-shadow: 0px 2px 8px rgba(33, 110, 251, 0.536331);
+          }
+
+          .login-main-img {
+            object-fit: cover;
+            height: 100%;
+          }
+        `}
+      </style>
+    </section>
+  );
+};
+
+export default Login;
